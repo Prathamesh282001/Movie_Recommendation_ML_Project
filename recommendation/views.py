fb9cedcf9c3e2de41db62015ee261807138b1778
Prathamesh282001 <chavan.prthm28@gmail.com>
2023-09-11 16:40:24 +0530

    third commit

--- recommendation/views.py
+++ recommendation/views.py
@@ -15,7 +15,11 @@ print(similarity_score)"""
 
 # Create your views here.
 def home(request):
-    return render(request,"index.html")
+    lst = movie_list.title
+    context = {
+                "movie_list":lst,
+               }
+    return render(request,"index.html",context)
 
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
